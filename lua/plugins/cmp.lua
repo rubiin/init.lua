ac02b52248d98f01dde23207754c6f1521118526
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-10 16:55:20 +0545

    fix: dont load uneeded snippets

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,8 +6,28 @@ return {
     end,
     config = function()
       require("luasnip.loaders.from_vscode").load {
-        exclude = { "javascript" },
-    }
+        include = { "javascript",
+          "css",
+          "docker",
+          "go",
+          "html",
+          "javascript",
+          "json",
+          "lua",
+          "markdown",
+          "python",
+          "rust",
+          "shell",
+          "sql",
+          -- "yaml",
+        },
+      }
+      require("luasnip").filetype_extend("typescript", { "tsdoc" })
+      require("luasnip").filetype_extend("javascript", { "jsdoc" })
+      require("luasnip").filetype_extend("lua", { "luadoc" })
+      require("luasnip").filetype_extend("python", { "pydoc" })
+      require("luasnip").filetype_extend("rust", { "rustdoc" })
+      require("luasnip").filetype_extend("sh", { "shelldoc" })
     end,
   },
   {
