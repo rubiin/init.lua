0925946d769252462d9da13fc7eba7bdcbec606e
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-10 16:17:33 +0545

    fix:Add a nvim-cmp source

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -14,7 +14,7 @@ return {
 
       local luasnip = require("luasnip")
       local cmp = require("cmp")
-
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
