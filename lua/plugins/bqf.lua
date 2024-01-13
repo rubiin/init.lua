808b7fc010a0281e74bbf89bb15b87229fb2f6d2
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-13 14:14:43 +0545

    feat: add bqf
    closes #9

---
+++ lua/plugins/bqf.lua
@@ -0,0 +1,14 @@
+return{
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf",
+    config = function()
+      require("bqf").setup {
+        auto_resize_height = false,
+        preview = {
+          auto_preview = false,
+        }
+      }
+    end
+  },
+}
