6290c8727c3b45870727cb3882a83b5af3e2fb1f
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-21 15:11:15 +0545

    Add VimEnter event to fortune.nvim plugin

--- lua/plugins/ui/fortune.lua
+++ lua/plugins/ui/fortune.lua
@@ -1,10 +1,10 @@
 return {
   'rubiin/fortune.nvim',
   lazy = true,
+  event = 'VimEnter',
   config = function()
     require('fortune').setup({
       max_width = 60,
     })
   end,
-  event = 'BufRead',
 }
