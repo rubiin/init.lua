a957bb296c4fa4f3e16894106b610f9172e5059b
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-01-28 14:41:35 +0545

    fix: rename sidebar to explorer

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- misc
-    -- { import = "lazyvim.plugins.extras.formatting.prettier" }, --replace with biome
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
---
+++ lua/plugins/editor/bufferline.lua
@@ -0,0 +1,17 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Explorer",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    }
+
+  }
+}
