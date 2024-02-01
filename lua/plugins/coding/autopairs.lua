2a0a233a2ebc879113ddd5036faa4a8de25a5d7e
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-01 17:06:03 +0545

    fix: support autopairs in cmp

--- lua/plugins/coding/autopairs.lua
+++ lua/plugins/coding/autopairs.lua
@@ -2,14 +2,32 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {
-      map_cr = true,
-      map_bs = true, -- map the <BS> key
-      map_c_h = false, -- Map the <C-h> key to delete a pair
-      map_c_w = false, -- map <c-w> to delete a pair if possible
-      disable_in_visualblock = true,
-      check_ts = true,
-      disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    dependencies = {
+      "hrsh7th/nvim-cmp",
     },
+    config = function()
+      -- import nvim-autopairs
+      local autopairs = require("nvim-autopairs")
+
+      -- configure autopairs
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        map_cr = true,
+        map_bs = true, -- map the <BS> key
+        map_c_h = false, -- Map the <C-h> key to delete a pair
+        map_c_w = false, -- map <c-w> to delete a pair if possible
+        disable_in_visualblock = true,
+        disable_filetype = { "TelescopePrompt", "spectre_panel" },
+      })
+
+      -- import nvim-autopairs completion functionality
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+      -- import nvim-cmp plugin (completions plugin)
+      local cmp = require("cmp")
+
+      -- make autopairs and completion work together
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
   },
 }
