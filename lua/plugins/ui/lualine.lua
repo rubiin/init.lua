34ce81756be45f1ff267fd69e4458e03cf86e91e
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-02 17:22:34 +0545

    fix: update lualine.lua to catch import error

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,19 +1,20 @@
 -- use a protected call so we don't error out on first use
 local status_ok, lualinestyles = pcall(require, "utils.helpers")
 if not status_ok then
-	print("please restart neovim")
+	print("Please restart neovim after setup")
 	return
 end
 
-local lualine_styles = lualinestyles.lualine_styles
 
-local used_style = lualine_styles.styles.slanted -- this is the default style, change it to the style you want to use from the list
+
+local used_style = lualinestyles.styles.slanted -- this is the default style, change it to the style you want to use from the list
+local lualine_styles = lualinestyles.lualine_styles(used_style)
 
 return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     lazy = true,
-    opts = lualine_styles(used_style),
+    opts = lualine_styles,
   },
 }
