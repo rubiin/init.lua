43e4d15a455f1b15b2401e8a900cef1a4c42b6be
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-05 12:21:21 +0545

    fix: icons

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,10 +1,10 @@
 -- use a protected call so we don't error out on first use
-local status_ok, lualinestyles = pcall(require, "utils.helpers")
-if not status_ok then
-  print("Please restart neovim after setup")
-  return
-end
+local success, lualinestyles = pcall(require, "utils.helpers")
 
+if not success then
+    print("Error loading 'utils.helpers':", lualinestyles)
+    -- Additional error handling code
+end
 local used_style = lualinestyles.styles.slanted -- this is the default style, change it to the style you want to use from the list
 local lualine_styles = lualinestyles.lualine_styles(used_style)
 
