f5212d5ef3e471ca440634ec97c2a6cf96e326bd
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-06 00:51:04 +0545

    feat: add treesj

---
+++ lua/plugins/coding/treesj.lua
@@ -0,0 +1,8 @@
+return {
+  "Wansmer/treesj",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("treesj").setup({--[[ your config ]]
+    })
+  end,
+}
--- lua/plugins/ui/dropbar.lua
+++ lua/plugins/ui/dropbar.lua
@@ -1,9 +1,7 @@
 return {
   {
     "Bekaboo/dropbar.nvim",
-    enabled = function()
-      return require("utils.helpers").is_neovim_version_satisfied(10)
-    end,
+    enabled = true,
     -- optional, but required for fuzzy finder support
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
