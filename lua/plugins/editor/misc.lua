8f65da7708b1f9178e7e673dacf7bb6812c1f12e
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-11 11:21:40 +0545

    fix: add denylist for vim illuminate

--- lua/plugins/editor/misc.lua
+++ lua/plugins/editor/misc.lua
@@ -24,6 +24,7 @@ return {
       manual_mode = false,
     },
   },
+  -- nvim notify
   {
     "rcarriga/nvim-notify",
     opts = {
@@ -32,10 +33,42 @@ return {
       stages = "fade_in_slide_out",
     },
   },
+  -- neo tree
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       popup_border_style = vim.g.border_style,
+    },
+  },
+
+  {
+    "RRethy/vim-illuminate",
+    opts = {
+      filetypes_denylist = {
+        "mason",
+        "harpoon",
+        "DressingInput",
+        "NeogitCommitMessage",
+        "qf",
+        "dirvish",
+        "oil",
+        "minifiles",
+        "fugitive",
+        "alpha",
+        "NvimTree",
+        "lazy",
+        "NeogitStatus",
+        "Trouble",
+        "netrw",
+        "lir",
+        "DiffviewFiles",
+        "Outline",
+        "Jaq",
+        "spectre_panel",
+        "toggleterm",
+        "DressingSelect",
+        "TelescopePrompt",
+      },
     },
   },
 }
