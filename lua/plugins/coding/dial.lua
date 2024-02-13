bf9d82dfa267ce15cc61bf216570397b6c375f35
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-13 12:14:27 +0545

    fix: replace boole with dial

---
+++ lua/plugins/coding/dial.lua
@@ -0,0 +1,80 @@
+-- Better increase/descrease
+return {
+  "monaqa/dial.nvim",
+  event = "VeryLazy",
+    -- stylua: ignore
+    keys = {
+      { "<C-a>", function() return require("dial.map").inc_normal("mygroup") end, expr = true, desc = "Increment" },
+      { "<C-x>", function() return require("dial.map").dec_normal("mygroup") end, expr = true, desc = "Decrement" },
+    },
+  config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group({
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.alias.bool,
+        augend.semver.alias.semver,
+      },
+      typescript = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.constant.new({ elements = { "let", "const" } }),
+        augend.constant.new({ elements = { "public", "private", "protected" } }),
+        augend.constant.new({ elements = { "any", "unknown" } }),
+        augend.constant.new({ elements = { "null", "undefined" } }),
+      },
+      visual = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.date.alias["%Y/%m/%d"],
+        augend.constant.alias.alpha,
+        augend.constant.alias.Alpha,
+        augend.constant.alias.Alpha,
+      },
+      mygroup = {
+        augend.constant.new({
+          elements = { "and", "or" },
+          word = true, -- if false, "sand" is incremented into "sor", "doctor" into "doctand", etc.
+          cyclic = true, -- "or" is incremented into "and".
+        }),
+        augend.constant.new({
+          elements = { "True", "False" },
+          word = true,
+          cyclic = true,
+        }),
+        augend.constant.new({
+          elements = { "public", "private" },
+          word = true,
+          cyclic = true,
+        }),
+        augend.constant.new({
+          elements = { "prod", "dev", "stage", "test" },
+          word = true,
+          cyclic = true,
+        }),
+        augend.constant.new({
+          elements = { "on", "off" },
+          word = true,
+          cyclic = true,
+        }),
+        augend.constant.new({
+          elements = { "include", "exclude" },
+          word = true,
+          cyclic = true,
+        }),
+        augend.constant.new({
+          elements = { "&&", "||" },
+          word = false,
+          cyclic = true,
+        }),
+        augend.date.alias["%m/%d/%Y"], -- date (02/19/2022, etc.)
+        augend.constant.alias.bool, -- boolean value (true <-> false)
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.semver.alias.semver,
+      },
+    })
+  end,
+}
