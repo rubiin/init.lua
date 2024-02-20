0bbfa4740af10304f090ee7db9ac62d1001fa72a
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-02-20 12:15:49 +0545

    fix: add gitblame by default

--- lua/plugins/editor/misc.lua
+++ lua/plugins/editor/misc.lua
@@ -2,6 +2,21 @@
 
 return {
   {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 200,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
+    },
+  },
+
+  {
     "akinsho/bufferline.nvim",
     opts = {
       options = {
