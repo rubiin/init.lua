377ee0fd8f7a92d95c410f4461b26950c79c0f88
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-02 02:38:40 +0545

    fix: disable capslocks

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -248,6 +248,24 @@ autocmd("FileType", {
   desc = "Disable `mini.indentscope` For Specific Filetypes",
 })
 
+-- Disable caps lock while vim is running
+-- FIX: fix this for wayland as WriteAllBuffers
+
+local capslock_augroup = utils.augroup("ToggleCapsLock")
+
+autocmd("InsertEnter", {
+  group = capslock_augroup,
+  pattern = "*",
+  command = "silent !setxkbmap -option",
+})
+
+autocmd("InsertLeave", {
+  pattern = "*",
+  group = capslock_augroup,
+  command = "silent !setxkbmap -option ctrl:nocaps",
+})
+
+
 -- ========================================================================== --
 -- ==                          USER COMMANDS                               == --
 -- ========================================================================== --
