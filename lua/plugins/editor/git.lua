7e065cfecfb28d67e994ab39eb095ca881eb7840
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-05 23:11:57 +0545

    fix: git stuffs

---
+++ lua/plugins/editor/git.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+  },
+  {
+    "akinsho/git-conflict.nvim",
+    init = function()
+      require("git-conflict").setup({
+        default_mappings = false,
+        disable_diagnostics = true,
+      })
+    end,
+    keys = {
+      { "<leader>gco", ":GitConflictChooseOurs<cr>" },
+      { "<leader>gct", ":GitConflictChooseTheirs<cr>" },
+      { "<leader>gcb", ":GitConflictChooseBoth<cr>" },
+      { "<leader>gc0", ":GitConflictChooseNone<cr>" },
+      { "]x", ":GitConflictNextConflict<cr>" },
+      { "[x", ":GitConflictPrevConflict<cr>" },
+    },
+  },
+}
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,12 +2,6 @@ return {
   { "tpope/vim-sleuth", event = "VeryLazy" }, -- This plugin automatically adjusts 'shiftwidth' and 'expandtab' heuristically based on the current file
   { "tpope/vim-repeat", event = "VeryLazy" }, -- This plugin enables . repeat support on some plugins
   { "ThePrimeagen/vim-be-good", event = "VeryLazy" }, -- This plugin is a game to help you get better at Vim
-  {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
-    opts = {},
-    keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
-  },
 }
 
 -- TODO: vim-fugitive
