e1e1c614137e286ca0bb46c7b3ded8d6a5bc281a
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-08 23:17:34 +0545

    fix: lazy disable options

--- init.lua
+++ init.lua
@@ -13,11 +13,11 @@
 -- ░▓        ▓
 -- ░▓▓▓▓▓▓▓▓▓▓
 -- ░░░░░░░░░░
-
 -- this is supposed to make loading faster
 vim.loader.enable()
 local prequire = require("utils").prequire
 
+package.loaded["lazyvim.config.options"] = true
 prequire("config.globals") -- user and lazy globals
 prequire("config.lazy")
 prequire("utils.filetype")
