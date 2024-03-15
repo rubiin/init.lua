34ae4405311a6950aedb2fa2901eadb44265aae5
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-15 14:48:51 +0545

    fix: lazygit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,7 +29,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.editor.leap" },
-    { import = "lazyvim.plugins.extras.util.gitui" },
     -- import/override with your plugins
     { import = "plugins" },
   },
