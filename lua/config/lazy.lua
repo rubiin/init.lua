ac05225188c43520bc9c3c6e5905b7d154217150
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-15 14:31:27 +0545

    fix: add gitui

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.editor.leap" },
+    { import = "lazyvim.plugins.extras.util.gitui" },
     -- import/override with your plugins
     { import = "plugins" },
   },
