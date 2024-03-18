f73393cf562706971caedb430954c0aeec9d328b
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-18 10:58:38 +0545

    feat: add go ftplugin

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,7 @@
+local utils = require("utils")
+--------------------------------------------------------------------------------
+
+-- fix my habits
+utils.ft_abbr("function", "func")
+utils.ft_abbr("local", "const")
+utils.ft_abbr("--", "//")
