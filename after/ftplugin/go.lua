445e4e7fa275929c75ced9d418353e2bf3031e34
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-25 01:46:16 +0545

    fix: run go

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -8,13 +8,16 @@ utils.ft_abbr("boolean", "bool")
 utils.ft_abbr("float", "float32")
 utils.ft_abbr("--", "//")
 
-
 vim.keymap.set("n", "<leader>f.", function()
-  local file = vim.fn.expand("%") -- Get the current file name
-    local escaped_file = vim.fn.shellescape(file) -- Properly escape the file name for shell commands
-    vim.cmd("!chmod +x " .. escaped_file) -- Make the file executable
-    vim.cmd("vsplit") -- Split the window vertically
-    vim.cmd("terminal " .. escaped_file) -- Open terminal and execute the file
-    vim.cmd("startinsert") -- Enter insert mode, recommended by echasnovski on Reddit
-
-end, { desc = "Execute current file in terminal (if it's a script)" })
+  local new_buffer = vim.api.nvim_create_buf(true, false)
+  local file = vim.fn.expand("%")
+  vim.api.nvim_buf_set_lines(new_buffer, 0, 1, false, { "Output" })
+  vim.fn.jobstart({ "go", "run", file }, {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        vim.api.nvim_buf_set_lines(new_buffer, -1, -1, false, data)
+      end
+    end,
+  })
+end, { buffer = true, desc="Run File with Go" })
