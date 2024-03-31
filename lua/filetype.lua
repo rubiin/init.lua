08c658c1d3a09fd651f8401223190aebfef261ab
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-03-31 21:19:45 +0545

    fix: remove defualt filetypes

--- lua/filetype.lua
+++ lua/filetype.lua
@@ -6,28 +6,18 @@
 
 vim.filetype.add({
   extension = {
-    profile = "sh",
-    rasi = "rasi",
     rofi = "rasi",
     wofi = "rasi",
-    env = "dotenv",
-    json = "jsonc",
     ejs = "html",
     htm = "html",
     mjml = "html",
     eta = "html",
-    conf = "conf",
-    hbs = "html",
     svx = "markdown",
     mdx = "markdown",
     svelte = "svelte",
-    norg = "norg",
-    patch = "patch",
   },
   filename = {
     [".env"] = "dotenv",
-    [".zprofile"] = "sh",
-    [".zshenv"] = "sh",
     ["tsconfig.tsbuildinfo"] = "jsonc",
     ["vifmrc"] = "vim",
     ["LICENCE"] = "markdown",
