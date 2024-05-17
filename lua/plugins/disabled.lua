3eac5f6a5587945061cd2eec283bbc593a9c6c0e
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-17 17:08:33 +0545

    fix: disable mini comment

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,7 +4,6 @@ local disable_plugins = require("utils").disable_plugins
 
 return disable_plugins({
   "folke/tokyonight",
-  "echasnovski/mini.pairs",
   "echasnovski/mini.comment",
   "iamcco/markdown-preview.nvim",
 })
