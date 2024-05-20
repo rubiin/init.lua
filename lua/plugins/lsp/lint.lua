f0c3f49bb864cbfb4ae378df679919d776ad3392
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-20 11:21:26 +0545

    refactor(lint): group eslint filetypes

--- lua/plugins/lsp/lint.lua
+++ lua/plugins/lsp/lint.lua
@@ -1,19 +1,23 @@
+local linters_by_ft = {
+  bash = { "shellcheck" },
+  markdown = { "markdownlint" },
+  lua = { "selene", "luacheck" },
+  sh = { "shellcheck" },
+  zsh = { "shellcheck" },
+}
+
+local eslint_file_types = { "javascript", "typescript", "javascriptreact", "typescriptreact", "sveltve" }
+
+local P = { "eslint_d" }
+for _, ft in pairs(eslint_file_types) do
+  linters_by_ft[ft] = P
+end
+
 return {
   {
     "mfussenegger/nvim-lint",
     opts = {
-      linters_by_ft = {
-        bash = { "shellcheck" },
-        markdown = { "markdownlint" },
-        lua = { "selene", "luacheck" },
-        javascript = { "eslint_d" },
-        typescript = { "eslint_d" },
-        javascriptreact = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
-        svelte = { "eslint_d" },
-        sh = { "shellcheck" },
-        zsh = { "shellcheck" },
-      },
+      linters_by_ft = linters_by_ft,
       linters = {
         selene = {
           condition = function(ctx)
