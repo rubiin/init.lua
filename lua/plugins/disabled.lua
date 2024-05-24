8fc35a96c31e5f19a435e36b6018773cb684ba29
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-24 15:23:35 +0545

    fix: remove mini pairs

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,5 +5,6 @@ local disable_plugins = require("utils").disable_plugins
 return disable_plugins({
   "folke/tokyonight",
   "echasnovski/mini.comment",
+  "echasnovski/mini.pairs",
   "iamcco/markdown-preview.nvim",
 })
