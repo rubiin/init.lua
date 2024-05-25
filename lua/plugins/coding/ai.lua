3e1b1c1209f0a500721d29adf6ece6a1ea229ad3
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-05-25 20:10:38 +0545

    fix(copilot): remove duplicate cmp

--- lua/plugins/coding/ai.lua
+++ lua/plugins/coding/ai.lua
@@ -41,23 +41,7 @@ return {
       })
     end,
   },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    lazy = true,
-    opts = {},
-    config = function(_, opts)
-      local copilot_cmp = require("copilot_cmp")
-      copilot_cmp.setup(opts)
-      -- attach cmp source whenever copilot attaches
-      -- fixes lazy-loading issues with the copilot cmp source
-      LazyVim.lsp.on_attach(function(client)
-        if client.name == "copilot" then
-          copilot_cmp._on_insert_enter({})
-        end
-      end)
-    end,
-  },
+
   {
     "nvim-lualine/lualine.nvim",
     optional = true,
