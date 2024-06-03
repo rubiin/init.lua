bb9a7ca2abc07564c3d61fa0b2ff01ab61242627
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-06-03 12:21:52 +0545

    fix: new horizontal keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,7 @@
 -- ========================================================================== --
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Not everything needs to be a keymap, you can also use `user_commands`
+-- https://vim.fandom.com/wiki/Unused_keys check this for unused keys
 
 local util = require("utils")
 local keymap, delete_keymap = util.keymap, util.delete_keymap
@@ -15,12 +16,6 @@ keymap("n", "<C-3>", "ggVGx", { desc = "Delete All" })
 
 -- Escape from insert mode with jj
 keymap("i", "jj", "<Esc>", { noremap = false })
-
--- Keeping the cursor centered.
-keymap("n", "<C-d>", "<C-d>zz", { desc = "Scroll Downwards" })
-keymap("n", "<C-u>", "<C-u>zz", { desc = "Scroll Upwards" })
-keymap("n", "n", "nzzzv", { desc = "Next Result" })
-keymap("n", "N", "Nzzzv", { desc = "Previous Result" })
 
 -- Block Arrow Keys
 keymap("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
@@ -53,27 +48,6 @@ keymap("n", "<leader>;l", "<cmd>LspInfo<CR>", { desc = "Lsp Info" })
 keymap("n", "<leader>;i", "<cmd>ConformInfo<CR>", { desc = "Conform Info" })
 keymap("n", "<leader>;c", LazyVim.news.changelog, { desc = "Changelog [LazyVim]" })
 keymap("n", "<leader>;M", vim.cmd.messages, { desc = "Display Messages" })
-
--- Make U opposite to u.
-keymap("n", "U", "<C-r>", { desc = "Redo" })
-
--- keeps registers clean
-
-keymap({ "n", "x" }, "x", '"_x')
--- Change text without putting it into the vim register,
--- see https://stackoverflow.com/q/54255/6064933
-keymap("n", "c", '"_c')
-keymap("n", "C", '"_C')
-keymap("n", "cc", '"_cc')
-keymap("x", "c", '"_c')
-
--- do not clutter the register if blank line is deleted
-keymap("n", "dd", function()
-  if vim.api.nvim_get_current_line():find("^%s*$") then
-    return '"_dd'
-  end
-  return "dd"
-end, { expr = true })
 
 -- Beginning and end of line in `:` command mode
 keymap("c", "<C-a>", "<home>")
@@ -112,3 +86,41 @@ end, { desc = "Add Author Details" })
 
 -- QUITTING
 keymap({ "n", "x" }, "<MiddleMouse>", vim.cmd.wqall, { desc = "Quit App" })
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+-- Additional keymaps(hacks which make using neovim more sane)
+
+-- Make U opposite to u.
+keymap("n", "U", "<C-r>", { desc = "Redo" })
+
+-- keeps registers clean
+
+keymap({ "n", "x" }, "x", '"_x')
+-- Change text without putting it into the vim register,
+-- see https://stackoverflow.com/q/54255/6064933
+keymap("n", "c", '"_c')
+keymap("n", "C", '"_C')
+keymap("n", "cc", '"_cc')
+keymap("x", "c", '"_c')
+
+-- do not clutter the register if blank line is deleted
+keymap("n", "dd", function()
+  if vim.api.nvim_get_current_line():find("^%s*$") then
+    return '"_dd'
+  end
+  return "dd"
+end, { expr = true })
+
+-- Keeping the cursor centered.
+keymap("n", "<C-d>", "<C-d>zz", { desc = "Scroll Downwards" })
+keymap("n", "<C-u>", "<C-u>zz", { desc = "Scroll Upwards" })
+keymap("n", "n", "nzzzv", { desc = "Next Result" })
+keymap("n", "N", "Nzzzv", { desc = "Previous Result" })
+
+-- Using H/L to go to the beginning and the end of line
+-- Note: H will map to _ (the first non-whitespace character of a line)
+-- It would be helpful if it is a indent line in some languages like Python, Ruby, YAML, ...
+keymap("n", "H", "_")
+keymap("n", "L", "$")
