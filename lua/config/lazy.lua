800ebd33d9825033bd6fbe77daa6047fcfbf29c4
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-06-09 11:56:12 +0545

    Revert "fix(theme): use tokyonight"
    This reverts commit 58f6a218f7bacdbc0da88a29261134545a1ea582.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -39,6 +39,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = '*', -- try installing the latest stable version for plugins that support semver
   },
+  install = { colorscheme = { "catppuccin" } },
   dev = { path = "~/personal-vim-plugs" },
   checker = {
     -- automatically check for plugin updates
