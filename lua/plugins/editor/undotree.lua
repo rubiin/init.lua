4928f9867bc3e85de14d7fc03339db93fd1442f4
Rubin Bhandari <roobin.bhandari@gmail.com>
2024-06-13 10:18:41 +0545

    fix: use undotree

---
+++ lua/plugins/editor/undotree.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "jiaoshijie/undotree",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = true,
+    keys = { -- load the plugin only when using it's keybinding:
+      { "<leader>cu", "<cmd>lua require('undotree').toggle()<cr>" },
+    },
+  },
+}
--- lua/plugins/ui/telescope.lua
+++ lua/plugins/ui/telescope.lua
@@ -87,37 +87,6 @@ return {
     },
   },
   {
-    "debugloop/telescope-undo.nvim",
-    dependencies = { -- note how they're inverted to above example
-      {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-      },
-    },
-    keys = {
-      {
-        "<leader>cu",
-        "<cmd>Telescope undo<cr>",
-        desc = "Undo History",
-      },
-    },
-    opts = {
-      -- don't use `defaults = { }` here, do this in the main telescope spec
-      extensions = {
-        undo = {
-          -- telescope-undo.nvim config, see below
-        },
-      },
-    },
-    config = function(_, opts)
-      -- Calling telescope's setup from multiple specs does not hurt, it will happily merge the
-      -- configs for us. We won't use data, as everything is in it's own namespace (telescope
-      -- defaults, as well as each extension).
-      require("telescope").setup(opts)
-      require("telescope").load_extension("undo")
-    end,
-  },
-  {
     "prochri/telescope-all-recent.nvim",
     dependencies = {
       "nvim-telescope/telescope.nvim",
