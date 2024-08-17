7832dd6dc4a27001184b75c7d6aed682bf972eb8
🤖 automated <cron@job>
2024-08-17 21:34:21 +0545

    devina  laptop 💻 (2)

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -54,64 +54,6 @@ return {
       { "<leader>uC", get_colorsheme, desc = "Colorscheme With Preview" },
     },
   },
-  -- add catppuccin
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    keys = {
-      { "<leader>uC", get_colorsheme, desc = "Colorscheme With Preview" },
-    },
-    opts = {
-      flavour = "mocha",
-      integrations = {
-        barbecue = {
-          dim_dirname = true, -- directory name is dimmed by default
-          bold_basename = true,
-          dim_context = false,
-          alt_background = false,
-        },
-        cmp = true,
-        dashboard = true,
-        dropbar = {
-          enabled = true,
-          color_mode = true, -- enable color for kind's texts, not just kind's icons
-        },
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        leap = true,
-        lsp_trouble = true,
-        lsp_saga = true,
-        mason = true,
-        markdown = true,
-        mini = {
-          enabled = true,
-        },
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        ufo = true,
-        rainbow_delimiters = true,
-        which_key = true,
-      },
-    },
-  },
   {
     "LazyVim/LazyVim",
     opts = {
