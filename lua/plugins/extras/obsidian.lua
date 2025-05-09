057c3b71e1b4000f50841a0b88309df99a40f929
🤖 automated <cron@job>
2025-05-09 17:25:52 +0545

    devina  laptop 💻 (2)

--- lua/plugins/extras/obsidian.lua
+++ lua/plugins/extras/obsidian.lua
@@ -1,8 +1,10 @@
 local vault_path = vim.fn.expand("~/Documents")
 
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   ft = "markdown",
+  version = "*", -- recommended, use latest release instead of latest commit
+  lazy = true,
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     -- Required.
@@ -15,28 +17,45 @@ return {
         path = vault_path,
       },
     },
-    picker = {
-      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
-      name = "fzf-lua",
-      -- Optional, configure key mappings for the picker. These are the defaults.
-      -- Not all pickers support all mappings.
-      mappings = {
-        -- Create a new note from your query.
-        new = "<C-x>",
-        -- Insert a link to the selected note.
-        insert_link = "<C-l>",
-      },
-    }, -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
+    -- Optional, if you keep notes in a specific subdirectory of your vault.
+    notes_subdir = "notes",
+    -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
     -- levels defined by "vim.log.levels.*".
     log_level = vim.log.levels.INFO,
-    templates = {
-      subdir = "templates",
+
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
       date_format = "%Y-%m-%d",
-      time_format = "%H:%M",
-      -- A map for custom variables, the key should be the variable and the value a function
-      substitutions = {},
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = nil,
+      -- Optional, if you want `Obsidian yesterday` to return the last work day or `Obsidian tomorrow` to return the next work day.
+      workdays_only = true,
     },
 
+    -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+    completion = {
+      -- Enables completion using nvim_cmp
+      nvim_cmp = false,
+      -- Enables completion using blink.cmp
+      blink = true,
+      -- Trigger completion at 2 chars.
+      min_chars = 2,
+    },
+
+    -- Where to put new notes. Valid options are
+    -- _ "current_dir" - put new notes in same directory as the current buffer.
+    -- _ "notes_subdir" - put new notes in the default notes subdirectory.
+    new_notes_location = "notes_subdir",
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
     note_id_func = function(title)
       -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
       -- In this case a note with the title 'My new note' will be given an ID that looks
@@ -53,5 +72,25 @@ return {
       end
       return tostring(os.time()) .. "-" .. suffix
     end,
+
+    templates = {
+      subdir = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
+    picker = {
+      -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+      name = "fzf-lua",
+      -- Optional, configure key mappings for the picker. These are the defaults.
+      -- Not all pickers support all mappings.
+      mappings = {
+        -- Create a new note from your query.
+        new = "<C-x>",
+        -- Insert a link to the selected note.
+        insert_link = "<C-l>",
+      },
+    },
   },
 }
