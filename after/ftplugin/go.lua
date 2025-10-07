1657a7c83146b0c05d8afdf878487f5244ae6452
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-10-07 18:11:03 +0545

    fix: remove trailing spaces in function abbreviation mappings across multiple language configurations

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -2,7 +2,7 @@ local utils = require("utils")
 --------------------------------------------------------------------------------
 
 -- fix my habits
-utils.ft_abbr("function ", "func ")
+utils.ft_abbr("function", "func")
 utils.ft_abbr("local", "var")
 utils.ft_abbr("boolean", "bool")
 utils.ft_abbr("float", "float32")
--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -7,7 +7,7 @@ local utils = require("utils")
 -- fix my habits
 utils.ft_abbr("cosnt", "const")
 utils.ft_abbr("local", "const")
-utils.ft_abbr("def ", "function ")
+utils.ft_abbr("def", "function")
 utils.ft_abbr("--", "//")
 utils.ft_abbr("~=", "!==")
 utils.ft_abbr("elseif", "else if")
