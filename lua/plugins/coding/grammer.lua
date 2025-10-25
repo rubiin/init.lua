012cc90cbf05b3626bc742f30ecb19ef3b27d45d
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-10-25 16:21:00 +0545

    chore: comment out harper_ls LSP settings in grammer.lua

--- lua/plugins/coding/grammer.lua
+++ lua/plugins/coding/grammer.lua
@@ -1,22 +1,22 @@
 -- check config here: https://writewithharper.com/docs/integrations/neovim
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        harper_ls = {
-          settings = {
-            ["harper-ls"] = {
-              userDictPath = "~/.config/cspell.json",
-              linters = {
-                SentenceCapitalization = false,
-                SpellCheck = false
-              }
-            }
-          },
-        },
-      }
-    }
-  }
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       harper_ls = {
+  --         settings = {
+  --           ["harper-ls"] = {
+  --             userDictPath = "~/.config/cspell.json",
+  --             linters = {
+  --               SentenceCapitalization = false,
+  --               SpellCheck = false
+  --             }
+  --           }
+  --         },
+  --       },
+  --     }
+  --   }
+  -- }
 }
