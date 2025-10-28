92dc3fd3f8f7bbe36c19a7da0823bfc833e190ec
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-10-28 21:18:52 +0545

    chore: add screenkey.nvim plugin config and refresh lazy-lock.json

---
+++ lua/plugins/extras/screen-key.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "NStefan002/screenkey.nvim",
+    lazy = false,
+    version = "*", -- or branch = "main", to use the latest commit
+    win_opts = {
+      border = vim.g.border_style,
+    },
+  },
+}
