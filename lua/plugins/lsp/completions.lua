5cb4912da0607bde113fd6e8c8f1a5e676e70390
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-12-08 23:48:27 +0545

    fix: update nvim-treesitter commit and move blink.cmp keymap options to completions.lua

--- lua/plugins/lsp/completions.lua
+++ lua/plugins/lsp/completions.lua
@@ -3,12 +3,31 @@ return {
   {
     "saghen/blink.cmp",
     dependencies = { "rafamadriz/friendly-snippets" },
-
     -- use a release tag to download pre-built binaries
     version = "1.*",
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
+      keymap = {
+        -- https://cmp.saghen.dev/configuration/keymap.html
+        preset = "none",
+        ["<CR>"] = { "select_and_accept", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+        ["<D-c>"] = { "show", "hide" },
+        ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
+        ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+        ["<D-g>"] = { "show_signature", "hide_signature" },
+      },
+      completion = {
+        documentation = { window = { border = vim.g.border_style } },
+        menu = {
+          border = vim.g.border_style,
+          draw = {
+            columns = { { "kind_icon", "label", gap = 2 }, { "label_description", "kind" } },
+          },
+        },
+      },
       sources = {
         providers = {
           snippets = {
--- lua/plugins/overrides/misc.lua
+++ lua/plugins/overrides/misc.lua
@@ -162,31 +162,6 @@ return {
       auto_integrations = true,
     },
   },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        -- https://cmp.saghen.dev/configuration/keymap.html
-        preset = "none",
-        ["<CR>"] = { "select_and_accept", "fallback" },
-        ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
-        ["<D-c>"] = { "show", "hide" },
-        ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
-        ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
-        ["<D-g>"] = { "show_signature", "hide_signature" },
-      },
-      completion = {
-        documentation = { window = { border = vim.g.border_style } },
-        menu = {
-          border = vim.g.border_style,
-          draw = {
-            columns = { { "kind_icon", "label", gap = 2 }, { "label_description", "kind" } },
-          },
-        },
-      },
-    },
-  },
 }
 
 -- TODO: check the hover doc issue
